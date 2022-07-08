0893239c806713fbe2ed939e45c08e1953f94a3a
Priyanshu29032004 <108617032+Priyanshu29032004@users.noreply.github.com>
2022-07-08 21:57:11 +0530

    Update settings.py

--- bloggers_universe/settings.py
+++ bloggers_universe/settings.py
@@ -126,10 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR , "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "static")
     
-# ]
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
