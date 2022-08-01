a1bfe86be48e1ce7dc932ae093ddd4240b9a431c
Priyanshu29032004 <108617032+Priyanshu29032004@users.noreply.github.com>
2022-08-01 19:03:41 +0530

    Update settings.py

--- bloggers_universe/settings.py
+++ bloggers_universe/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,7 +99,7 @@ WSGI_APPLICATION = 'bloggers_universe.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+DATABASES['default'] =  dj_database_url.config()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
